9584d4d1e488a822eadcab832ac5a0fce35a70b6
philipp-hellwig <philipp.hellwig@student.uva.nl>
2025-08-10 11:21:41 +0200

    nvm

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setLibrary("md", mdLib);
 
   return {
-    pathPrefix: "/philipp-hellwig.github.io/",
     dir: {
       input: "src",
       includes: "_includes",
